413a4bd88d49b9b4d47539ddd8df4f2af69f5343
anaBeatrizWalker <anabiawalker@gmail.com>
2024-10-21 16:10:20 -0300

    Concluindo desafio: adicionando validação no botão

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,18 @@
 import styled from 'styled-components';
+import { IButtonContainerStyled } from './types';
 
 
-export const ButtonContainer = styled.button`
+export const ButtonContainer = styled.button<IButtonContainerStyled>`
     width: 100%;
     height: 42px;
-    background-color: #81259D;
+    background-color: ${(props) => props.disableButton ? '#B57BC7' : '#81259D'};
     color: #FFF;
 
-    border: 1px solid #81259D;
+    border: 1px solid ${(props) => props.disableButton ? '#B57BC7' : '#81259D'};
     border-radius: 21px;
 
     &:hover {
-        opacity: 0.6;
-        cursor:pointer;
+        opacity:${(props) => props.disableButton ? '' : '0.6'};
+        cursor: ${(props) => props.disableButton ? 'not-allowed' : 'pointer'};
     }
 `
--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,4 +1,9 @@
 export interface IButtonProps {
     title: string;
     onClick?: React.MouseEventHandler<HTMLButtonElement>;
+    disableButton?: boolean;
+}
+
+export interface IButtonContainerStyled{
+    disableButton?: boolean;
 }
